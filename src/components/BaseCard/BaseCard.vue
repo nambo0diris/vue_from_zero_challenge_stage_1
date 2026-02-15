6b658c3766d18f988ee8bbe518b2fce3da535305
Anzor Khaindrava <namboodiris@Anzors-MacBook-Pro.local>
2026-02-15 08:56:17 +0300

    day_6

---
+++ src/components/BaseCard/BaseCard.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <section>
+        <slot name="title">Default title</slot>
+        <slot name="body"/>
+        <slot name="actions"/>
+    </section>
+</template>
--- src/components/CounterDashboard/CounterDashboard.vue
+++ src/components/CounterDashboard/CounterDashboard.vue
@@ -2,6 +2,7 @@
 import {ref} from 'vue'
 import BaseButton from "../ui/BaseButton.vue"
 import BaseInput from "../ui/BaseInput.vue"
+import BaseCard from '../BaseCard/BaseCard.vue';
 
 const name = ref<string>("");
 
@@ -13,6 +14,17 @@ const click = () => {
 
 <template>
     <section> 
+        <BaseCard>
+            <template #title>
+                <h1>Тайтл</h1>
+            </template>
+            <template #body>
+                <div>Боди</div>
+            </template>
+            <template #actions>
+                <BaseButton label="Button" variant="primary" :disabled="false" @click="click" />
+            </template>
+        </BaseCard>
         <p>{{ name }}</p>
         <BaseButton label="Button" variant="primary" :disabled="false" @click="click" />
         <BaseInput type="text" placeholder="Введите" v-model="name"/>
