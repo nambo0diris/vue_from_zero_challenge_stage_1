9fc643c75eee2b96dde35f510846d9991ff31c6d
Anzor Khaindrava <namboodiris@Anzors-MacBook-Pro.local>
2026-02-15 09:51:20 +0300

    day_7

--- src/components/CounterDashboard/CounterDashboard.vue
+++ src/components/CounterDashboard/CounterDashboard.vue
@@ -5,15 +5,24 @@ import BaseInput from "../ui/BaseInput.vue"
 import BaseCard from '../BaseCard/BaseCard.vue';
 
 const name = ref<string>("");
-
+const dashes = ref<string[]>([])
 const click = () => {
-   console.log("pushed")
+   if (name.value) {
+    dashes.value = [...dashes.value, name.value]
+    name.value = ""
+   }
 }
 </script>
 
 
 <template>
     <section> 
+        <div v-if="dashes.length">
+            <div v-for="dash in dashes" :key="dash">
+                {{ dash }}
+            </div>
+        </div>
+        <div v-else>Пока нет бордов</div>
         <BaseCard>
             <template #title>
                 <h1>Тайтл</h1>
@@ -22,11 +31,10 @@ const click = () => {
                 <div>Боди</div>
             </template>
             <template #actions>
-                <BaseButton label="Button" variant="primary" :disabled="false" @click="click" />
+                <BaseInput type="text" placeholder="Введите название дэшборда" v-model="name"/>
+                <BaseButton label="Создать дэшборд" variant="primary" :disabled="false" @click="click" />
             </template>
         </BaseCard>
         <p>{{ name }}</p>
-        <BaseButton label="Button" variant="primary" :disabled="false" @click="click" />
-        <BaseInput type="text" placeholder="Введите" v-model="name"/>
     </section>
 </template>
